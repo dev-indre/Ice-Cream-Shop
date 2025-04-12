0aa83c2bbecaf376e5802055e913371335901ed7
dev-indre <slezaiteindre@gmail.com>
2025-04-12 16:17:05 +0300

    first commit nav+header

---
+++ ice_cream_shop/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+
+export default function App() {
+  return (
+    <div className="background-hero-header">
+      <div className="background-ice-cream">
+    <Header/>
+    <Hero/>
+    </div>
+    </div>
+
+  )
+}
---
+++ ice_cream_shop/src/UI/Button.jsx
@@ -0,0 +1,6 @@
+
+export default function Button({background, children}) {
+  return <button className="btn"
+  style={{background}}>
+      {children}</button>
+}
